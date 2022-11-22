e573b04f4f836f08f2b7474506e031484c126ab7
MemoTonui <tonuilinda@gmail.com>
2022-11-22 18:38:48 +0100

    Fix spacing on Feedback Card

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name, title, img }) => (
-  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+  <div className="flex justify-between flex-col px-5 py-5 rounded-[20px] max-w-[390px] md:mr-10 sm:mr-5 mr-0  feedback-card">
     <img
       src={quotes}
       alt="double_quotes"
